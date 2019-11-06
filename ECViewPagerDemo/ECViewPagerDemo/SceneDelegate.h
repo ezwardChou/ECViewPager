ad28c57053aecebc1fd9ea88b9e36d6d91d33fd3
Edward Chou <ezwardChou>
2019-11-06 17:44:16 +0800

    工程原型创建

---
+++ ECViewPagerDemo/ECViewPagerDemo/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  ECViewPagerDemo
+//
+//  Created by Chou Edward  on 2019/11/6.
+//  Copyright © 2019 Chou Edward . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
